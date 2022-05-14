8cb0e4d2c5c45ea9e3c8fba0292682b090137122
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 12:33:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        
+        public static double Chain(double input, Func<double,double>[] fs)
+        {
+            for (int i = 0; i < fs.Length; i++)
+                input = fs[i](input);
+            return input;
+        }
+        
+
+    }
+
+}
